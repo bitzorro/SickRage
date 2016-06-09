ca38c60c224196745f2afb537a1f44880cf34f08
Rato <rato.aq2@gmail.com>
2016-06-09 21:00:34 +0200

    Adding initial unit tests for guessit name parser

---
+++ tests/guessit_tests.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+"""
+Guessit name parser tests
+"""
+import os
+import unittest
+import yaml
+
+from nose_parameterized import parameterized
+from sickbeard.name_parser.guessit_parser import parser
+
+
+class GuessitTests(unittest.TestCase):
+    files = {
+        'tvshows': 'tvshows.yml',
+    }
+
+    properties = {
+        'original_name': None,
+        'series_name': None,
+        'season_number': None,
+        'release_group': None,
+        'air_date': None,
+        'version': -1,
+        'extra_info': None,
+        'episode_numbers': [],
+        'ab_episode_numbers': [],
+    }
+
+    parameters = []
+    for scenario_name, file_name in files.iteritems():
+        with open(os.path.join('datasets', file_name), 'r') as stream:
+            data = yaml.safe_load(stream)
+
+        for release_name, release_values in data.iteritems():
+            expected = dict(properties)
+            expected.update(release_values)
+            expected['original_name'] = release_name
+            parameters.append([scenario_name, release_name, expected])
+
+    @parameterized.expand(parameters)
+    def test_guess(self, scenario_name, release_name, expected):
+        actual = parser.guess(release_name)
+        print('Testing {scenario_name}: {release_name}'.format(scenario_name=scenario_name, release_name=release_name))
+        self.assertEqual(expected, actual)
